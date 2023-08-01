ea88d9ed1694f78781c1df93e35c6a8195d3d00c
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-01 22:14:16 +0300

    added player and obstacle, made the background and obstacle move, player can now jump

---
+++ Personal Work/Create with Code - Course by Unity/Project Unit 3 - Sounds and Effects/Assets/_Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // variables
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
---
+++ Personal Work/Create with Code - Course by Unity/Project Unit 3 - Sounds and Effects/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityModifier = 1.5f;
+    private bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;        
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
