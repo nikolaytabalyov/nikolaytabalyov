2a3c92ac77cac3424d2c580fd487b945dc6268ef
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-12 19:40:54 +0300

    Imported sprites for player and enemy and created an enemy

--- Personal Work/Create with Code - Course by Unity/Personal Project/Assets/_Scripts/PlayerController.cs
+++ Personal Work/Create with Code - Course by Unity/Personal Project/Assets/_Scripts/PlayerController.cs
@@ -1,48 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D playerRb;
-    // variables
-    public float speed = 10;
-    public float jumpForce = 100;
-    private bool onGround = true;
-    public float hInput;
+    public enum State{
+        InAir,
+        OnGround
+    }
+    
+    [SerializeField] private float decelerationSpeed;
+    [SerializeField] private State jumpState = State.OnGround;
+    [SerializeField] private float speed = 25.0f;
+    [SerializeField] private float jumpForce;
+    private bool _jumpPressed = false;
+    private float _horizontalInput;
+    private Rigidbody2D _playerRb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // get components
-        playerRb = GetComponent<Rigidbody2D>();
+    private void Start() {
+        _playerRb = GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+            _jumpPressed = true;
+        if (Input.GetKeyUp(KeyCode.Space))
+            _jumpPressed = false;
+        _horizontalInput = Input.GetAxis("Horizontal");        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        hInput = Input.GetAxis("Horizontal");
+    private void FixedUpdate() {
+        if (_jumpPressed && jumpState == State.OnGround) 
+            Jump();
+        MoveLeftOrRight();
     }
 
-    void FixedUpdate()
-    {
-        MovePlayer();
-    }
-    void MovePlayer()
-    {
-        //playerRb.velocity = new Vector2(speed * hInput * Time.deltaTime, playerRb.velocity.y);
-
-        playerRb.AddForce(Vector2.right * speed * hInput, ForceMode2D.Impulse);
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
-        {
-            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            onGround = false;
+    private void MoveLeftOrRight() {
+        if (_horizontalInput == 1 || _horizontalInput == -1) {
+            _playerRb.AddForce(Vector2.right * speed * _horizontalInput, ForceMode2D.Impulse);
+        } 
+        else if (_horizontalInput == 0) {
+            _playerRb.velocity = new Vector2(_playerRb.velocity.x * decelerationSpeed, _playerRb.velocity.y);
         }
+
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-            onGround = true;
+    private void Jump() {
+        _playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        jumpState = State.InAir;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) 
+            jumpState = State.OnGround;
     }
 }
