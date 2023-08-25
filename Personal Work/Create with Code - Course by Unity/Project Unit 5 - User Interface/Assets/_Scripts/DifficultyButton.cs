47a05f3e3463860812d0804124f7e223c6dce674
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-25 16:40:03 +0300

    Added title screen with working difficulty buttons

---
+++ Personal Work/Create with Code - Course by Unity/Project Unit 5 - User Interface/Assets/_Scripts/DifficultyButton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NikolayTabalyov {
+    public class DifficultyButton : MonoBehaviour {
+
+        [Header("Components")]
+        private Button _difficultyButton;
+        private GameManager _gameManager;
+        [Header("Variables")]
+        [SerializeField] private int _difficulty;
+
+        #region Unity Methods
+        private void Awake() {
+            _difficultyButton = GetComponent<Button>();
+            _difficultyButton.onClick.AddListener(SetDifficulty);
+        }
+
+        private void Start() {
+            _gameManager = GameManager.Instance;
+        }
+        #endregion
+
+        #region Other Methods
+        private void SetDifficulty() {
+            Debug.Log(gameObject.name + " was clicked");
+            _gameManager.StartGame(_difficulty);
+        }
+        #endregion
+    }
+}
--- Personal Work/Create with Code - Course by Unity/Project Unit 5 - User Interface/Assets/_Scripts/GameManager.cs
+++ Personal Work/Create with Code - Course by Unity/Project Unit 5 - User Interface/Assets/_Scripts/GameManager.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 namespace NikolayTabalyov 
 {
     public class GameManager : MonoBehaviour {
     
-        [Header("Variables")]
+        [Header("Components")]
         [SerializeField] private List<GameObject> _targets = new();
         [SerializeField] private TextMeshProUGUI _scoreText;
         [SerializeField] private TextMeshProUGUI _gameOverText;
+        [SerializeField] private Button _restartButton; 
+        [SerializeField] private GameObject _titleScreen;
+        [Header("Variables")]
         private int _score;
         private float _spawnRate = 1.0f;
         public State GameState { get; private set; }
@@ -23,22 +28,24 @@ namespace NikolayTabalyov
             GameOver
         }
 
+        #region Unity Methods
         private void Awake() {
-            if (Instance == null) {
-                Instance = this;
-            } else {
+            if (Instance != null && Instance != this) {
                 Destroy(gameObject);
+            } else {
+                Instance = this;
             }
         }
-        private void Start() {
-            GameState = State.Running;
-            _score = 0;
-            UpdateScore(0);
-            StartCoroutine(SpawnTarget());
+        #endregion 
+
+        #region Other Methods
+        public void RestartGame() {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
         public void GameOver() {
             GameState = State.GameOver;
+            _restartButton.gameObject.SetActive(true);
             _gameOverText.gameObject.SetActive(true);
         }
 
@@ -47,6 +54,17 @@ namespace NikolayTabalyov
             _scoreText.text = "Score: " + _score;
         }
 
+        public void StartGame(int difficulty) {
+            GameState = State.Running;
+            _score = 0;
+            UpdateScore(0);
+            // diff = Easy -> spawn every 1s, diff = Normal -> spawn every 0.66s, diff = Hard -> spawn every 0.45s
+            _spawnRate /=  difficulty == 1 ? difficulty : difficulty == 2 ? difficulty - 0.5f : difficulty - 0.8f;
+            StartCoroutine(SpawnTarget());
+            _titleScreen.gameObject.SetActive(false);
+        }
+
+        #endregion
         IEnumerator SpawnTarget() {
             while (GameState == State.Running) {
                 yield return new WaitForSeconds(_spawnRate);
