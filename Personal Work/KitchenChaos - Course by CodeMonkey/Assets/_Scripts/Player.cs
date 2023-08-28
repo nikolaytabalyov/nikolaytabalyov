84cfc76856d4033ff3b8fd6746e943ba3ffc0ca1
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-28 17:27:59 +0300

    added collision checking and basic clear counter

--- Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/Player.cs
+++ Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/Player.cs
@@ -4,6 +4,8 @@ namespace NikolayTabalyov {
     public class Player : MonoBehaviour {
         
         [Header("Variables")]
+        private float PLAYER_RADIUS = 0.7f;
+        private float PLAYER_HEIGHT = 2f;
         [SerializeField] private float _speed = 5f;
         private float rotationSpeed = 10f;
         private bool _isWalking;
@@ -14,7 +16,7 @@ namespace NikolayTabalyov {
         #region Unity Methods
         private void Update() {
             if (IsWalking()) {
-                MovePlayer();
+                HandlePlayerMovement();
                 RotatePlayer();
             }
                 
@@ -23,8 +25,15 @@ namespace NikolayTabalyov {
 
         #region Movement Methods
         
-        private void MovePlayer() {
-            transform.position += GetDirection() * _speed * Time.deltaTime;
+        private void HandlePlayerMovement() {
+            float moveDistance = _speed * Time.deltaTime;
+            if (!DoCapsuleCast(GetDirection(), moveDistance)) {
+                transform.position += GetDirection() * moveDistance;
+            } else if (!DoCapsuleCast(GetDirection('x'), moveDistance)) {
+                transform.position += GetDirection('x') * moveDistance;
+            } else if (!DoCapsuleCast(GetDirection('z'), moveDistance)) {
+                transform.position += GetDirection('z') * moveDistance;
+            }
         }
         private void RotatePlayer() {
             transform.forward = Vector3.Slerp(transform.forward, GetDirection(), rotationSpeed * Time.deltaTime);
@@ -32,6 +41,9 @@ namespace NikolayTabalyov {
         #endregion
 
         #region Return Value Methods
+        private bool DoCapsuleCast(Vector3 direction, float moveDistance) {
+            return Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PLAYER_HEIGHT, PLAYER_RADIUS, direction, moveDistance);
+        }
 
         public bool IsWalking() {
             this._isWalking = _gameInputManager.GetInputVectorNormalized() != Vector2.zero;
@@ -42,6 +54,18 @@ namespace NikolayTabalyov {
             Vector2 input = _gameInputManager.GetInputVectorNormalized();
             return new Vector3(input.x, 0, input.y);
         }
+        private Vector3 GetDirection(char axis) {
+            Vector2 input = _gameInputManager.GetInputVectorNormalized();
+            if (axis == 'x') {
+                return new Vector3(input.x, 0, 0).normalized;
+            } else if (axis == 'z') {
+                return new Vector3(0, 0, input.y).normalized;
+            } else {
+                return Vector3.zero;
+                    
+            }
+        }
+        
         #endregion
     }
 }
