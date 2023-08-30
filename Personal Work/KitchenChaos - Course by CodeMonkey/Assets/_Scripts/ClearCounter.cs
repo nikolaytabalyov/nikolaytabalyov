899c9da13c1ea22adeee20c66e3df41d9b756e61
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-30 11:44:01 +0300

    you can change the kitchen object parent

--- Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/ClearCounter.cs
+++ Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/ClearCounter.cs
@@ -7,15 +7,39 @@ namespace NikolayTabalyov
         [Header("Components")]
         [SerializeField] private KitchenObjectSO _kitchenObjectSO;
         [SerializeField] private Transform _counterTopPoint;
+        [SerializeField] private ClearCounter _secondClearCounter;
         private KitchenObject _kitchenObject;
-        public void Interact() {
-            if (_kitchenObject is null) {
-                Transform kitchenObject = Instantiate(_kitchenObjectSO.prefab, _counterTopPoint.position, Quaternion.identity);
-                _kitchenObject = kitchenObject.GetComponent<KitchenObject>();
-                _kitchenObject.ClearCounter = this;
-            } else {
-                Debug.Log(_kitchenObject.ClearCounter);
+
+        [Header("Variables")]
+        [SerializeField] private bool _testing;
+
+        #region Unity Methods
+        private void Update() {
+            if (_testing && Input.GetKeyDown(KeyCode.Space)) {
+                if (_kitchenObject is not null) {
+                    _kitchenObject.SetNewClearCounter(_secondClearCounter);
+                }
             }
         }
+        #endregion
+
+        #region Other Methods
+        public void Interact() {
+            if (_kitchenObject is null) {
+                Transform kitchenObjectTransform = Instantiate(_kitchenObjectSO.prefab, _counterTopPoint);
+                kitchenObjectTransform.GetComponent<KitchenObject>().SetNewClearCounter(this);
+            } else {
+                Debug.Log(_kitchenObject.GetClearCounter());
+            }
+        }
+
+        public Transform GetNewCounterTopPoint() {
+            return _counterTopPoint;
+        }
+        public void SetKitchenObject(KitchenObject kitchenObject) => _kitchenObject = kitchenObject;
+        public KitchenObject GetKitchenObject() => _kitchenObject;
+        public void ClearKitchenObject() => _kitchenObject = null;
+        public bool HasKitchenObject() => _kitchenObject is not null;
+        #endregion
     }
 }
--- Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/KitchenObject.cs
+++ Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/KitchenObject.cs
@@ -5,7 +5,28 @@ namespace NikolayTabalyov
     public class KitchenObject : MonoBehaviour {
         
         [SerializeField] private KitchenObjectSO _kitchenObjectSO;
-        public ClearCounter ClearCounter { get; set; }
+        private ClearCounter _clearCounter;
         public KitchenObjectSO GetKitchenObjectSOName => _kitchenObjectSO;
+
+
+        public void SetNewClearCounter(ClearCounter clearCounter) {
+            if (_clearCounter is not null) {
+                Debug.Log("Clearing counter");
+                _clearCounter.ClearKitchenObject();
+            }
+            _clearCounter = clearCounter;
+
+            if (_clearCounter.HasKitchenObject()) {
+                Debug.LogError("ClearCounter already has a KitchenObject");
+            } 
+            _clearCounter.SetKitchenObject(this);
+
+            transform.parent = _clearCounter.GetNewCounterTopPoint();
+            transform.localPosition = Vector3.zero;            
+        }
+
+        public ClearCounter GetClearCounter() {
+            return _clearCounter;
+        }
     }
 }
