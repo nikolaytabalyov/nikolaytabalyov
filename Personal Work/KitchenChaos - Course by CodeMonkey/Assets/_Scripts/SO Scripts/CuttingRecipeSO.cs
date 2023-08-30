e4f3d981c52df856576680c8b859a24ba4f730f3
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-30 20:16:13 +0300

    added SO for 2 sliced variants and 3 cutting recipes

---
+++ Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/SO Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace NikolayTabalyov
+{
+	[CreateAssetMenu(fileName = "CuttingRecipeSO", menuName = "ScriptableObjects/CuttingRecipeSO", order = 2)]
+	public class CuttingRecipeSO : ScriptableObject {
+		
+        #region Variables
+        public KitchenObjectSO input;
+        public KitchenObjectSO output;
+		#endregion
+        
+		#region Methods
+        
+		#endregion
+        
+	}
+}
