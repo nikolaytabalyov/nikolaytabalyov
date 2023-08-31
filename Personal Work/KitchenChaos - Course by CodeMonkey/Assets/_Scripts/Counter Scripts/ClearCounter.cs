3f5861836a6a3790b62b2b5922a6318ac57e4b23
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-08-31 21:12:47 +0300

    player can add ingredient to plate that is on clear counter

--- Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/Counter Scripts/ClearCounter.cs
+++ Personal Work/KitchenChaos - Course by CodeMonkey/Assets/_Scripts/Counter Scripts/ClearCounter.cs
@@ -23,8 +23,8 @@ namespace NikolayTabalyov {
                     break;
 
                 case true: // if counter is not empty
-                    if (player.HasKitchenObject() && player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plate)) { 
-                        TryAddIngredientToPlate(plate);
+                    if (player.HasKitchenObject()) { // if player is holding something
+                        TryAddIngredientToCounter(player);
                     } else if (!player.HasKitchenObject()) { // if counter is not empty and player is not holding anything
                         GetKitchenObject().SetNewKitchenObjectParent(player);
                     }
@@ -32,6 +32,19 @@ namespace NikolayTabalyov {
             }
         }
 
+
+        private bool TryAddIngredientToCounter(Player player) {
+            if (GetKitchenObject().TryGetPlate(out PlateKitchenObject plateOnCounter)) { // if there is a plate on counter
+                if (plateOnCounter.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO)) {;
+                    player.GetKitchenObject().DestroySelf();
+                    return true;
+                } else {
+                    return false;
+                }
+            } else { // if there is no plate on counter
+                return false;
+            }
+        }
         private bool TryAddIngredientToPlate(PlateKitchenObject plate) {
             if (plate.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO)) {
                 GetKitchenObject().DestroySelf();
